43bcac9852b5af0b3da884c38b083933a80609dc
Nokeen <cordrepayne@gmail.com>
2022-09-21 21:27:23 -0500

    getChat route now returns the messages between two users and/or creates a chat between the two if none exists

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/User.model");
+const ChatLog = require("../models/ChatLog.model");
 
 const getFriends = (req, res) => {
 }
@@ -21,16 +22,36 @@ const getUsers = (req, res) => {
 
 const getChat = (req, res) => {
     const { friend, current } = req.body
-
-    User.findOne({_id: current})
-    .then(user => {
-        console.log(user)
-    })
-    User.findOne({username: friend})
-    .then(user => {
-        console.log(user)
-    })
-    res.status(201).json({current});
+    try {
+        User.findOne({_id: current})
+        .then(user1 => {
+            User.findOne({username: friend})
+            .then(user2 => {
+                const idSet = [user1.id, user2.id]
+                ChatLog.findOne({user_ids: idSet})
+                    .then(chatLog => {
+                        if(chatLog){
+                            const { message } = chatLog;
+                            const chat = { message };
+                            res.status(201).json({ chat: chat });
+                        }
+                        if(!chatLog){
+                            return ChatLog.create({user_ids: idSet})
+                        }
+                    }).then(createdChat => {
+                        if(createdChat){
+                            const { message } = createdChat;
+                            const chat = { message };
+                            res.status(201).json({ chat: chat });
+                        }
+                    })
+            })
+        })
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).json({message: error});
+    }
 }
 
 module.exports = {
