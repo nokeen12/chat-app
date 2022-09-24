94131ae58d2d574e17d71a4bdd81b6ff7d9a1d4c
Nokeen <cordrepayne@gmail.com>
2022-09-23 23:22:40 -0500

    Created route for sending messages

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -3,6 +3,15 @@ const ChatLog = require("../models/ChatLog.model");
 const Message = require("../models/Message.model");
 
 const sendMessage = (req, res) => {
+    const { chat_log, message, user_id } = req.body;
+
+    const newMessage = Message.create({chat_log: chat_log, message: message, user_id: user_id})
+    ChatLog.findById(chat_log)
+    .then(chatLog => chatLog.chat_log.push(newMessage._id).save())
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: err})
+    })
 }
 
 const editMessage = (req, res) => {
