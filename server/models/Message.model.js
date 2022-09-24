5ac185f3f5967cceaa68dda5fd7ffc492813523f
Nokeen <cordrepayne@gmail.com>
2022-09-23 22:48:20 -0500

    chatlog and user id now required for messages

--- server/models/Message.model.js
+++ server/models/Message.model.js
@@ -2,8 +2,17 @@ const { Schema, model } = require("mongoose");
 
 const messageSchema = new Schema(
   {
+    chat_log: {
+      type: Schema.Types.ObjectId,
+      ref: 'ChatLog',
+      required: true
+    },
     message: {
       type: String,
+      required: true,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
       required: true,
     }
   },
