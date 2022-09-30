9f8bfff791210e40301f7eb066086527de5f4753
Nokeen <cordrepayne@gmail.com>
2022-09-29 23:05:42 -0500

    Created base code for a global chat using websocket

--- server/server.js
+++ server/server.js
@@ -6,6 +6,26 @@ require("./db");
 const app = express();
 const PORT = process.env.PORT || 5005;
 
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: '4000' })
+
+wss.on("connection", function connection(ws){
+  console.log("A new client Connected!")
+  ws.send("Welcome New Client!"); //ws send goes to the client
+
+  ws.on('message', function incoming(data, isBinary){
+    console.log('received %s', data)
+    
+    wss.clients.forEach((client)=>{
+        if(client !== ws && client.readyState === WebSocket.OPEN){
+            client.send(data, { binary: isBinary });
+        }
+    })
+  }); 
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
